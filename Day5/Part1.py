859e4169bb06be8adc66018c0f47a4ebd362ac66
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-12-08 19:05:48 +0530

    Finish Day5

---
+++ Day5/Part1.py
@@ -0,0 +1,35 @@
+# full_input = open("test.txt").read().split("\n")
+full_input = open("input.txt").read().split("\n")
+
+S = []
+for line in full_input:
+    if line == '':
+        break
+    sz = (len(line)+1)//4
+    while len(S) < sz:
+        S.append([])
+    for i in range(len(S)):
+        ch = line[1+4*i]
+        if ch != ' ' and 'A'<=ch<='Z':
+            S[i].append(ch)
+
+for x in S:
+    x.reverse()
+
+found = False
+for line in full_input:
+    if line=='':
+        found = True
+        continue
+    if not found:
+        continue
+    cmd = line.split()
+    # print(cmd)
+    for i in range(int(cmd[1])):
+        n = S[int(cmd[3])-1][-1]
+        S[int(cmd[3])-1].pop()
+        S[int(cmd[5])-1].append(n)
+    # print(S)
+
+top = [x[-1] for x in S]
+print(''.join(top))
